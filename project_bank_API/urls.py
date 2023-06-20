bdfb61a441ee5a252278b8cf4e66e0306e1bad8f
sairanjansahu <sairanjansahu2000@gmail.com>
2023-06-20 13:50:27 +0530

    first commit

---
+++ project_bank_API/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for project_bank_API project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from app.views import *
+from rest_framework.routers import DefaultRouter
+DRO=DefaultRouter()
+
+DRO.register('branch',branch_jd,basename='branch')
+DRO.register('bank',bank_jd,basename='bank')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('bank_upload_data/',bank_upload_data,name='bank_upload_data'),
+    path('branch_upload_data/',branch_upload_data,name='branch_upload_data'),
+
+    path('app/',include(DRO.urls)),
+]
